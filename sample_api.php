51e5cbb2c2f1426805bf55817be5bb111adf2801
femie15 <33351638+femie15@users.noreply.github.com>
2022-01-07 17:43:17 +0100

    second
    sec

--- sample_api.php
+++ sample_api.php
@@ -5,18 +5,15 @@ $database = "example_database";
 $table = "todo_list";
 $response=array();
 try {
-  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
-//  echo "<h2>TODO</h2><ol>";
+  $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password); 
 header("Content-Type: JSON");
 $i=1;
   foreach($db->query("SELECT content FROM $table") as $row) {
 $response[$i]['item_id']= $row['item_id'];
 $response[$i]['content']= $row['content'];
-$i++;
-// echo "<li>" . $row['content'] . "</li>";
+$i++; 
   }
-echo json_encode($response,JSON_PRETTY_PRINT);
- // echo "</ol>";
+echo json_encode($response,JSON_PRETTY_PRINT); 
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
